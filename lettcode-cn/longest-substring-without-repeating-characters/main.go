156b0034a933838ab43e287430bb189888bd7048
maynard.liu <415074476@qq.com>
2025-02-17 14:06:51 +0800

    add 5

--- lettcode-cn/longest-substring-without-repeating-characters/main.go
+++ lettcode-cn/longest-substring-without-repeating-characters/main.go
@@ -2,19 +2,17 @@ package main
 
 import "fmt"
 
-
-
 func lengthOfLongestSubstring(s string) int {
 	if len(s) == 0 {
 		return 0
 	}
-	var  start, end int
+	var start, end int
 	end = 1
 	longest := 1
-	for i:=1; i< len(s);i++ {
+	for i := 1; i < len(s); i++ {
 		found := false
 		var j int
-		for j =start;j<end;j++ {
+		for j = start; j < end; j++ {
 			if s[j] == s[i] {
 				found = true
 				break
@@ -24,32 +22,50 @@ func lengthOfLongestSubstring(s string) int {
 			if s[i] == s[start] {
 				start++
 			} else {
-				if  end-start > longest {
-					longest = end-start
+				if end-start > longest {
+					longest = end - start
 				}
-				start = j+1
+				start = j + 1
 			}
 		}
 		end++
 	}
-	if  end-start > longest {
-		longest = end-start
+	if end-start > longest {
+		longest = end - start
 	}
 	return longest
 }
 
+func lengthOfLongestSubstring2(s string) int {
+	window := make([]rune, 0)
+	result := 0
+	for _, v := range s {
+		for i, vv := range window {
+			if v == vv {
+				window = window[i+1:]
+			}
+		}
+		window = append(window, v)
+
+		if len(window) > result {
+			result = len(window)
+		}
+	}
+
+	return result
+}
 func main() {
-	tests := []struct{
-		Nums string
+	tests := []struct {
+		Nums     string
 		Expected int
-	} {
-		{ "abcabcbb" ,3 },
-		{ "bbbbb" ,1 },
-		{ "pwwkew" ,3 },
-		{ "" ,0 },
+	}{
+		{"abcabcbb", 3},
+		{"bbbbb", 1},
+		{"pwwkew", 3},
+		{"", 0},
 	}
 	for _, t := range tests {
-		if lengthOfLongestSubstring(t.Nums) !=  t.Expected {
+		if lengthOfLongestSubstring(t.Nums) != t.Expected {
 			fmt.Println("expected:", t.Expected, " real:", lengthOfLongestSubstring(t.Nums), t.Nums)
 		}
 	}
